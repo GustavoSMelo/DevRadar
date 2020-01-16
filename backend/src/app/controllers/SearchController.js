2263c01ff947504448914cfae4d9db333f8c4b8d
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-01-16 17:57:55 -0300

    removing some query requests

--- backend/src/app/controllers/SearchController.js
+++ backend/src/app/controllers/SearchController.js
@@ -4,9 +4,9 @@ const user = require('../models/users');
 module.exports = {
     async index(req, res) {
         const Op = Sequelize.Op;
-        const { longitude, latitude, techs } = req.query;
+        const { techs } = req.query;
 
-        if (!longitude || !latitude || !techs) {
+        if (!techs) {
             return res
                 .status(400)
                 .json({ Error: 'Some query params is null ' });
