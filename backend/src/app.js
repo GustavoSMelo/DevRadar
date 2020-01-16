87efc52ca83e3e2785585b90fdf59f80b4d1ac97
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-01-15 23:29:40 -0300

    Change of position the cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,9 +4,9 @@ const cors = require('cors');
 const routes = require('./routes/routes');
 require('./database/index');
 
+app.use(cors());
 app.use(express.json());
 app.use('/', routes);
-app.use(cors());
 
 const PORT = 3333;
 app.listen(PORT, () =>
