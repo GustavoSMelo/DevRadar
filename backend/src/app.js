8fe1ac94187875d6158f4283c7f2fa407f39e5a1
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-01-17 19:06:00 -0300

    add websocket http

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,12 +3,18 @@ const app = express();
 const cors = require('cors');
 const routes = require('./routes/routes');
 require('./database/index');
+const http = require('http');
+const { setupWebsocket } = require('./websocket');
+
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 app.use(cors());
 app.use(express.json());
 app.use('/', routes);
 
 const PORT = 3333;
-app.listen(PORT, () =>
+server.listen(PORT, () =>
     console.log('Server is running in http://localhost:3333 ')
 );
