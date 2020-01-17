77bcfeaf390f433ee04a44f0b4dd99f93ed86baa
Gustavo Santos Melo <gsantos15569@gmail.com>
2020-01-17 19:08:06 -0300

    ignore some warnings in rn

--- mobile/App.js
+++ mobile/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Routes from './src/routes/routes';
-import { StatusBar } from 'react-native';
+import { StatusBar, YellowBox } from 'react-native';
+
+YellowBox.ignoreWarnings(['Unrecognized WebSocket connection']);
 export default function App() {
     return (
         <>
